ce616c90f4a8e41153085b7cb7b4414b3fc8ec54
Mona Lisafreyashell <mche526@aucklanduni.ac.nz>
2025-02-04 17:33:19 +1300

    Ming-api-article.js

--- backend/src/routes/api/api.js
+++ backend/src/routes/api/api.js
@@ -3,7 +3,10 @@ import express from "express";
 const router = express.Router();
 
 // import child routes
-import tempRoutes from "./api-temp.js";
-router.use("/messages", tempRoutes);
+import articleRouter from "./api-article.js";
+router.use("/article", articleRouter);
+
 
 export default router;
+
+
